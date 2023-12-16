604b8c91755c62f3729e4ada4b75c5d93d237ba8
Marc raphael <marcraphael979@gmail.com>
2023-12-16 23:35:56 +0100

    Config and fix linter

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -8,11 +10,11 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 greetings = [
-  "Hello!",
-  "Hi there!",
-  "Greetings!",
-  "Welcome!",
-  "Good day!"
+  'Hello!',
+  'Hi there!',
+  'Greetings!',
+  'Welcome!',
+  'Good day!'
 ]
 
 greetings.each do |greeting|
